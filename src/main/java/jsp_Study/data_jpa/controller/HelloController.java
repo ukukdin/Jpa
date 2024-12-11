5359d6cfc544794368e6bbeba016726dbe979303
엄인국 <eum714211@gmail.com>
2024-12-11 22:54:13 +0900

    first jpa commit

---
+++ src/main/java/jsp_Study/data_jpa/controller/HelloController.java
@@ -0,0 +1,13 @@
+package jsp_Study.data_jpa.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
---
+++ src/test/java/jsp_Study/data_jpa/DataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package jsp_Study.data_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
